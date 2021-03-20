7b9d178d4ccdbd0fd3dde5f67fec460a46ec6b86
radyvoniukkate <radyvoniukkate@gmail.com>
2021-03-20 11:39:12 +0200

    Good update database asp

--- Food.WebApi/Food.EFData/DataContext.cs
+++ Food.WebApi/Food.EFData/DataContext.cs
@@ -17,5 +17,24 @@ namespace Food.EFData
         {
 
         }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<AppUserRole>(userRole =>
+            {
+                userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
+
+                userRole.HasOne(ur => ur.Role)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.RoleId)
+                    .IsRequired();
+
+                userRole.HasOne(ur => ur.User)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.UserId)
+                    .IsRequired();
+            });
+        }
     }
 }
